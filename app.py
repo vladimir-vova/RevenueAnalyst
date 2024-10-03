4fc0b4638af77468794d2f3c8254efe2af96b676
Vladimir <baimieff.volodya1@yandex.ru>
2024-10-03 22:35:49 +0500

    Соединение к БД

--- app.py
+++ app.py
@@ -4,3 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+
+
+def get_conn():
+    HOST = 'ep-green-butterfly-a26qo6f3.eu-central-1.aws.neon.tech'
+    PORT = 5432
+    DATABASE = 'datadb'
+    USER = 'datadb_owner'
+    PASSWORD = 'BmV3l0GArdNT'
+
+    connection = psycopg2.connect(
+        host = HOST,
+        port = PORT,
+        database = DATABASE,
+        user = USER,
+        password = PASSWORD,
+    )
+    return connection
+
+connection = get_conn()
